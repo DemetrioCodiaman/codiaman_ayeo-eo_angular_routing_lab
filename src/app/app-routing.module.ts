d6f17d800f60a080aeb5b3e0394b0aa143aee197
Demetrio Codiaman <20216950@s.ubaguio.edu>
2025-03-06 14:43:46 +0800

    edit on approuting module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { ProfileComponent } from './about/profile/profile.component';
-
+import { ContactComponent } from './contact/contact.component';
+import { adminGuard } from './admin/admin.guard';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent, children: [
-      { path: 'profile', component: ProfileComponent }
-    ]},
-    { path: 'contact', component: ContactComponent },
-    { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
-  ];
- 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { 
+    path: 'about', component: AboutComponent, children: [
+      { path: 'profile', component: ProfileComponent } // Fixed the missing path syntax
+    ]
+  },
+  { path: 'contact', component: ContactComponent },
+  { 
+    path: 'admin', 
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), 
+    canActivate: [adminGuard] 
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), HomeComponent, 
-    AboutComponent, ContactComponent, ProfileComponent],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
-
