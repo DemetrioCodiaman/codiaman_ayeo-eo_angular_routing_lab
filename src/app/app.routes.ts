d895f7d654d4313a14fa229a649e319e193653c9
x <x@f112.ub.lan>
2025-03-11 15:05:45 +0800

    Commiting chages in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './about/profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
+import { NgModule } from '@angular/core';
+import path from 'path';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent , children: [
+        { path: 'profile', component: ProfileComponent }]},
+    { path: 'contact', component: ContactComponent }];
+
+    @NgModule({
+        imports: [RouterModule.forRoot(routes)],  // Make sure RouterModule is correctly imported
+        exports: [RouterModule]  // Ensure RouterModule is exported
+      })
+    
+    export class AppRoutingModule { }
