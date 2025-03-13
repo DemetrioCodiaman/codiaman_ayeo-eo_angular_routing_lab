6a3f3c31e7fb75c69f474ca29b35283b6ba6c8b1
x <x@f112.ub.lan>
2025-03-13 15:25:03 +0800

    commiting modifyed codes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
+import { ChildOneComponent } from './child-one/child-one.component';
+import { ChildTwoComponent } from './child-two/child-two.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'parent', // Path for the parent route
+        component: ParentComponent, // Component for the parent route
+        children: [ // Define child routes under the 'children' array
+            {
+                path: 'child-one', // Path for the first child route (relative to 'parent')
+                component: ChildOneComponent // Component for the first child route
+            },
+            {
+                path: 'child-two', // Path for the second child route (relative to 'parent')
+                component: ChildTwoComponent // Component for the second child route
+            },
+            {
+                path: '', // Default child route (when only '/parent' is accessed)
+                redirectTo: 'child-one', // Redirect to 'child-one' by default
+                pathMatch: 'full' // Match the full path to redirect correctly
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/parent', // Redirect to '/parent' as the default route
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
